5e1c631bd9ceab61b7150be7959afb08c722291e
sugetha24 <sugetha24@gmail.com>
2013-06-28 07:33:09 -0400

    test

---
+++ add_test.py
@@ -0,0 +1,15 @@
+#!usr/bin/ python
+
+num1 = -1
+num2 = -2
+num3=0;
+if((num1%1) == 0)and((num2%1) == 0):
+	pass
+else:
+	print "number wrong";
+if(num1 > 0)and(num2 > 0):
+	num3=num1+num2;
+else:
+	print "number wrong";
+print num1, num2;
+
